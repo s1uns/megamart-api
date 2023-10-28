1799ae6513f66d1beed328ad96171e0f9595537f
Illia Teliuk <illiateliuk@gmail.com>
2023-10-28 22:21:03 +0300

    Created task model with basic fields

---
+++ Core/Models/Task.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Models
+{
+    public class Task
+    {
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public string Type { get; set; }
+    }
+}
