360f19ef7904c13b00f6d0fcc575f6d87c02a354
Illia Teliuk <illiateliuk@gmail.com>
2023-11-04 20:55:08 +0200

    Created generic repository and special model for the methods result. Maybe delete later

---
+++ Core/Models/Result.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Models
+{
+    public class Result<T>
+    {
+        public bool IsSuccessful { get; }
+        public string Message { get; }
+        public T Data { get; }
+
+        public Result(bool isSuccessful, T data)
+        {
+            IsSuccessful = isSuccessful;
+            Data = data;
+        }
+
+        public Result(bool isSuccessful, string message)
+        {
+            IsSuccessful = isSuccessful;
+            Message = message;
+        }
+
+        public Result(bool isSuccessful)
+        {
+            IsSuccessful = isSuccessful;
+        }
+    }
+}
---
+++ DAL/Repository/Interface/IRepository.cs
@@ -0,0 +1,24 @@
+﻿using Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Repository.Interface
+{
+    public interface IRepository<T> where T : BaseEntity
+    {
+        Task<Result<List<T>>> GetAllAsync();
+
+        Task<Result<T>> GetByIdAsync(int id);
+
+        Task<Result<T>> GetByPredicateAsync(Func<T, bool> predicate);
+
+        Task<Result<bool>> AddAsync(T entity);
+
+        Task<Result<bool>> UpdateAsync(int id, T newEntity);
+
+        Task<Result<bool>> DeleteAsync(int id);
+    }
+}
