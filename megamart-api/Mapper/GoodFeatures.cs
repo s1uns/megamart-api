a281f8029dc28c07e4f196a0818a05a0afce5fb3
Illia Teliuk <illiateliuk@gmail.com>
2023-11-20 23:33:48 +0200

    Added mapper profiles for good and category

---
+++ megamart-api/Mapper/GoodFeatures.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using Core.Models;
+using Infrustructure.Dto.Categories;
+using Infrustructure.Dto.Goods;
+
+namespace megamart_api.Mapper
+{
+    public class GoodFeatures : Profile
+    {
+        public GoodFeatures()
+        {
+            CreateMap<CreateGoodDto, Good>();
+            CreateMap<EditGoodDto, Good>();
+            CreateMap<Good, GoodShortInfoDto>()
+                .ForMember(c => c.SellerName, otp => otp.MapFrom(src => src.Seller.Name));
+        }
+    }
+}
