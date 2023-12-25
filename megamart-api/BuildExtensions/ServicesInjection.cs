003da7193f2db4dc0a7b4524d64889d2ccc65166
Illia Teliuk <illiateliuk@gmail.com>
2023-12-25 20:32:52 +0200

    Registered good service

--- megamart-api/BuildExtensions/ServicesInjection.cs
+++ megamart-api/BuildExtensions/ServicesInjection.cs
@@ -4,6 +4,8 @@ using BLL.Services.GenericService.Interfaces;
 using BLL.Services.GenericService;
 using DAL.Repository.Interface;
 using DAL.Repository;
+using BLL.Services.GoodManagement.Interfaces;
+using BLL.Services.GoodManagement;
 
 namespace megamart_api.BuildExtensions
 {
@@ -13,6 +15,7 @@ namespace megamart_api.BuildExtensions
         {
             services.AddScoped(typeof(IGenericService<>), typeof(GenericService<>));
             services.AddScoped<ICategoryService, CategoryService>();
+            services.AddScoped<IGoodService, GoodService>();
             services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
         }
     }
