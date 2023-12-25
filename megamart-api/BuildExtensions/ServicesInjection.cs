dfd6491d2b43262ef8bfbe045e9fc6e7f5810493
Illia Teliuk <illiateliuk@gmail.com>
2023-12-25 20:29:17 +0200

    Added builder extensions

---
+++ megamart-api/BuildExtensions/ServicesInjection.cs
@@ -0,0 +1,19 @@
+﻿using BLL.Services.CategoryManager.Interfaces;
+using BLL.Services.CategoryManager;
+using BLL.Services.GenericService.Interfaces;
+using BLL.Services.GenericService;
+using DAL.Repository.Interface;
+using DAL.Repository;
+
+namespace megamart_api.BuildExtensions
+{
+    internal static class ServicesInjection
+    {
+        internal static void AddServices(this IServiceCollection services)
+        {
+            services.AddScoped(typeof(IGenericService<>), typeof(GenericService<>));
+            services.AddScoped<ICategoryService, CategoryService>();
+            services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
+        }
+    }
+}
