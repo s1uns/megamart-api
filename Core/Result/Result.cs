8f36f1af3664ce40edfb635e030b0ce79894d054
Illia Teliuk <illiateliuk@gmail.com>
2024-02-01 21:59:05 +0200

    Implemented result pattern

---
+++ Core/Result/Result.cs
@@ -0,0 +1,39 @@
+﻿namespace Core.Result
+{
+    public class Result<TValue, TError>
+    {
+        public readonly TValue? _value;
+        public readonly TError? _error;
+
+        public readonly bool _isSuccess;
+
+        private Result(TValue value)
+        {
+            _isSuccess = true;
+            _value = value;
+            _error = default;
+        }
+
+        private Result(TError error)
+        {
+            _isSuccess = false;
+            _value = default;
+            _error = error;
+        }
+
+        //happy path
+        public static implicit operator Result<TValue, TError>(TValue value) => new Result<TValue, TError>(value);
+
+        //error path
+        public static implicit operator Result<TValue, TError>(TError error) => new Result<TValue, TError>(error);
+
+        public Result<TValue, TError> Match()
+        {
+            if (_isSuccess)
+            {
+                return _value!;
+            }
+            return _error!;
+        }
+    }
+}
