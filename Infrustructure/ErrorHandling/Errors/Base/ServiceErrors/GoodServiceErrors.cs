0775329d13b51e250dd80b2af289e3fc9781a7e2
Illia Teliuk <illiateliuk@gmail.com>
2024-02-01 21:59:26 +0200

    Created error returning type and specified it for the GoodService

---
+++ Infrustructure/ErrorHandling/Errors/Base/ServiceErrors/GoodServiceErrors.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrustructure.ErrorHandling.Errors.Base.ServiceErrors
+{
+    public static class GoodServiceErrors
+    {
+        public static readonly Error AddGoodError = new Error("Add Good Error", "Failed to add new good");
+        public static readonly Error EditGoodError = new Error("Edit Good Error", "Failed to edit the good");
+        public static readonly Error DeleteGoodError = new Error("Delete Good Error", "Failed to delete the good");
+        public static readonly Error GetGoodsError = new Error("Get All Goods Error", "Failed to get the goods");
+        public static readonly Error GetGoodError = new Error("Get Good Error", "Failed to get the good");
+        public static readonly Error WrongSellerError = new Error("Wrong Seller Error", "You are not the seller of this good");
+    }
+}
