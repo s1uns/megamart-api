4b1e381df8a733779492cbcec8fb270233b3e6f6
Illia Teliuk <illiateliuk@gmail.com>
2024-02-22 00:09:06 +0200

    Added errors for Identity services

---
+++ Infrustructure/ErrorHandling/Errors/ServiceErrors/IdentityServiceErrors.cs
@@ -0,0 +1,14 @@
+﻿using Infrustructure.ErrorHandling.Errors.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrustructure.ErrorHandling.Errors.ServiceErrors
+{
+    public static class IdentityServiceErrors
+    {
+        public static Error CreateAccountError(string errorMessage) => new Error("Create Account Error", $"Failed to create new account: {errorMessage}");
+    }
+}
