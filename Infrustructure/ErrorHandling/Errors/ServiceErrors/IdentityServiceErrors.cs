0026bf6166cbebcd1f07b3def63c9bd8b57c0803
Illia Teliuk <illiateliuk@gmail.com>
2024-02-29 21:48:58 +0200

    Added Errors and Exceptions for authentication/authorization services

--- Infrustructure/ErrorHandling/Errors/ServiceErrors/IdentityServiceErrors.cs
+++ Infrustructure/ErrorHandling/Errors/ServiceErrors/IdentityServiceErrors.cs
@@ -10,5 +10,8 @@ namespace Infrustructure.ErrorHandling.Errors.ServiceErrors
     public static class IdentityServiceErrors
     {
         public static Error CreateAccountError(string errorMessage) => new Error("Create Account Error", $"Failed to create new account: {errorMessage}");
+        public static Error CreateCustomerError(string errorMessage) => new Error("Create Customer Error", $"Failed to create new customer: {errorMessage}");
+        public static Error CreateSellerError(string errorMessage) => new Error("Create Seller Error", $"Failed to create new seller: {errorMessage}");
+        public static Error SignInError(string errorMessage) => new Error("Sign In Error", $"Failed to sign in: {errorMessage}");
     }
 }
