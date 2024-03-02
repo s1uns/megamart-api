97fa6316b74317bbef11c035a0db3b241f32f09b
Illia Teliuk <illiateliuk@gmail.com>
2024-03-02 22:18:36 +0200

    Updated order model

--- BLL/Services/OrderManagement/OrderService.cs
+++ BLL/Services/OrderManagement/OrderService.cs
@@ -1,6 +1,4 @@
-﻿/*using AutoMapper;
-using BLL.Services.DeliveryAddressManagement;
-using BLL.Services.GenericService;
+﻿using AutoMapper;
 using BLL.Services.OrderManagement.Interfaces;
 using Core.Models;
 using DAL.Repository.Interface;
@@ -8,10 +6,14 @@ using Microsoft.Extensions.Logging;
 
 namespace BLL.Services.OrderManagement
 {
-    public class OrderService : GenericService<Order>, IOrderService
+    public class OrderService : IOrderService
     {
         private readonly ILogger<OrderService> _logger;
         private readonly IMapper _mapper;
-        public OrderService(IRepository<Order> repository, ILogger<OrderService> logger, IMapper mapper) : base(repository, logger, mapper) { }
+        public OrderService(IRepository<Order> repository, ILogger<OrderService> logger, IMapper mapper)
+        {
+            _logger = logger;
+            _mapper = mapper;
+        }
     }
-}*/
+}
--- BLL/Services/OrderPositionManagement/OrderPositionService.cs
+++ BLL/Services/OrderPositionManagement/OrderPositionService.cs
@@ -1,17 +1,18 @@
-﻿/*using AutoMapper;
-using BLL.Services.DeliveryAddressManagement;
-using BLL.Services.GenericService;
+﻿using AutoMapper;
 using BLL.Services.OrderPositionManagement.Interfaces;
 using Core.Models;
-using DAL.Repository.Interface;
 using Microsoft.Extensions.Logging;
 
 namespace BLL.Services.OrderPositionManagement
 {
-    public class OrderPositionService : GenericService<OrderPosition>, IOrderPositionService
+    public class OrderPositionService : IOrderPositionService
     {
         private readonly ILogger<OrderPositionService> _logger;
         private readonly IMapper _mapper;
-        public OrderPositionService(IRepository<OrderPosition> repository, ILogger<OrderPositionService> logger, IMapper mapper) : base(repository, logger, mapper) { }
+        public OrderPositionService(ILogger<OrderPositionService> logger, IMapper mapper) 
+        { 
+            _logger = logger;
+            _mapper = mapper;
+        }
     }
-}*/
+}
