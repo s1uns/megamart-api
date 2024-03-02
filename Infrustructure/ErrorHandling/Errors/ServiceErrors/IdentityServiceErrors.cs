7aa78d88ea6138c574c4a34899cdea97ae3c15b4
Illia Teliuk <illiateliuk@gmail.com>
2024-03-02 22:23:05 +0200

    Errors and Exceptions for changing the password

--- Infrustructure/ErrorHandling/Errors/ServiceErrors/IdentityServiceErrors.cs
+++ Infrustructure/ErrorHandling/Errors/ServiceErrors/IdentityServiceErrors.cs
@@ -9,9 +9,13 @@ namespace Infrustructure.ErrorHandling.Errors.ServiceErrors
 {
     public static class IdentityServiceErrors
     {
-        public static Error CreateAccountError(string errorMessage) => new Error("Create Account Error", $"Failed to create new account: {errorMessage}");
+        public static readonly Error InvalidUserEmail = new Error("Invalid User Email Error", "User's email is invalid");
+        public static readonly Error WrongOldPasswordError = new Error("Wrong Old Password Error", "The old password is wrong");
+        public static Error CreateAccountError(string errorMessage) => new Error("Create Account Error", errorMessage);
         public static Error CreateCustomerError(string errorMessage) => new Error("Create Customer Error", $"Failed to create new customer: {errorMessage}");
         public static Error CreateSellerError(string errorMessage) => new Error("Create Seller Error", $"Failed to create new seller: {errorMessage}");
         public static Error SignInError(string errorMessage) => new Error("Sign In Error", $"Failed to sign in: {errorMessage}");
+        public static Error ResetPasswordError(string errorMessage) => new Error("Reset Password Error", $"Failed to reset your password: {errorMessage}");
+        public static Error InvalidNewPasswordError(string errorMessage) => new Error("Invalid New Password Error", errorMessage);
     }
 }
