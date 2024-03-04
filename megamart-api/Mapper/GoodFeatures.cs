83b96ac781b262db8b29244a88c052f02423e84d
Illia Teliuk <illiateliuk@gmail.com>
2024-03-04 18:32:04 +0200

    Created and updated mapping profiles

--- megamart-api/Mapper/GoodFeatures.cs
+++ megamart-api/Mapper/GoodFeatures.cs
@@ -14,15 +14,16 @@ namespace megamart_api.Mapper
                 .ForMember(g => g.GoodOptions, otp => otp.MapFrom(src => src.GoodOptions));
 
             CreateMap<EditGoodDto, Good>();
+
             CreateMap<Good, GoodShortInfoDto>()
                 .ForMember(g => g.SellerName, otp => otp.MapFrom(src => src.Seller.Name))
                 .ForMember(g => g.Categories, otp => otp.MapFrom(src => src.Categories))
                 .ForMember(g => g.GoodOptions, otp => otp.MapFrom(src => src.GoodOptions));
+
             CreateMap<Good, GoodFullInfoDto>()
                 .ForMember(g => g.SellerName, otp => otp.MapFrom(src => src.Seller.Name))
                 .ForMember(g => g.Categories, otp => otp.MapFrom(src => src.Categories))
                 .ForMember(g => g.GoodOptions, otp => otp.MapFrom(src => src.GoodOptions));
-
 
             CreateMap<GoodOptionDto, GoodOption>();
             CreateMap<GoodOption, GoodOptionDto>();
