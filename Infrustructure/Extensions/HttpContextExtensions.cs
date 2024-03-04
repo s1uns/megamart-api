ba70f4e06c8cf752d1c2ec1d90c1b174327ccdb8
Illia Teliuk <illiateliuk@gmail.com>
2024-03-04 18:27:28 +0200

    Moved httpcontext extension to Infrastructure project

--- Infrustructure/Extensions/HttpContextExtensions.cs
+++ Infrustructure/Extensions/HttpContextExtensions.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Http;
 using System.Security.Claims;
 
 
-namespace BLL.Extensions
+namespace Infrustructure.Extensions
 {
     public static class HttpContextExtensions
     {
